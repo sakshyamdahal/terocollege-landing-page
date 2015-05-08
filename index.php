478f3c3f0baeeeb3ec8270be0aab263e259ef558
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-05-08 12:24:26 -0500

    set sendGrid for email

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php include_once("index.html"); ?>
+<?php 
+require("sendgrid-php/sendgrid-php.php");
+include_once("index.html"); 
+?>
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+require("sendgrid-php/sendgrid-php.php");
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -9,18 +10,32 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
+$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
+$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
+$to = $_ENV['TO']; 
+
+$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+$email    = new SendGrid\Email();
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'dahalsakshyam@gmail.com'; 
+
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@terocollege.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$headers .= "Reply-To: $email_address";   
+
+// Create the email and send the message
+$email->addTo($to)->
+       setFrom($to)->
+       setSubject($email_subject)->
+       setText($email_body)->
+       addHeader($headers);
+
+
+$response = $sendgrid->send($email);
 return true;			
 ?>
